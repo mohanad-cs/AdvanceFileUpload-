be5e7c1b3cf5590b6828d2484dfd81e0d9579763
Mohanad Shamsan <85135152+mohanad-cs@users.noreply.github.com>
2025-04-24 17:59:42 +0300

    Add session status checker worker and related services
    Introduce `SessionStatusCheckerWorker` to manage session status checking and logging. Implement `SessionsStatusCheckerService` to process file upload sessions older than 48 hours, marking them as failed. Add `FileUploadSessionFailedEvent` to represent session failure events. These changes improve monitoring and management of file upload sessions.

---
+++ src/AdvanceFileUpload/AdvanceFileUpload.API/SessionStatusCheckerWorker.cs
@@ -0,0 +1,29 @@
+﻿
+using AdvanceFileUpload.Application;
+
+namespace AdvanceFileUpload.API
+{
+    public class SessionStatusCheckerWorker : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<SessionStatusCheckerWorker> _logger;
+        public SessionStatusCheckerWorker(IServiceProvider serviceProvider, ILogger<SessionStatusCheckerWorker> logger)
+        {
+            _serviceProvider = serviceProvider;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stating Session status checker worker...");
+           var scope= _serviceProvider.CreateScope();
+           var sessionChecker= scope.ServiceProvider.GetRequiredService<SessionsStatusCheckerService>();
+           await sessionChecker.StartAsync(cancellationToken);
+
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Session status checker worker stopped.");
+            return Task.CompletedTask;
+        }
+    }
+}
